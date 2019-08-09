949599b6021131118a3848ab12ae962be730e1a8
Niko Achilles Kokkinos <nikolaos.kokkinos@vorwerk.de>
2019-08-09 04:41:01 +0200

    completed task 2

---
+++ json-schema-solutions/part1/task2/validate.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Ajv = require('ajv');
+var assert = require('assert');
+
+var humanData = require('./human.json');
+var machineData = require('./machine.json');
+var invalidData = require('./invalid.json');
+
+var humanMachineSchema = require('./human_machine.schema.json');
+
+var ajv = Ajv();
+
+var validate = ajv.compile(humanMachineSchema);
+
+assert(validate(humanData));
+assert(validate(machineData));
+invalidData.forEach(function (data, i) {
+  assert(!validate(data), '#' + i + ' should be invalid: ' + JSON.stringify(data));
+});
+
+console.log('You\'ve done task 2!');
