a78fdb672dab57f7e6bc647ba852a5a88c057187
Niko Achilles Kokkinos <nikolaos.kokkinos@vorwerk.de>
2019-08-13 08:18:20 +0200

    removed test env.

--- json-schema-solutions/part1/task3/validate.js
+++ json-schema-solutions/part1/task3/validate.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-var Ajv = require('ajv');
-var assert = require('assert');
+var Ajv = require("ajv");
+var assert = require("assert");
 
-var dateArgsSchema = process.env.NODE_ENV == 'test'
-                      ? require('../answers/task3_date_args_schema')
-                      : require('./date_args_schema');
+var dateArgsSchema = require("./date_args_schema");
 
 var ajv = Ajv();
 var validate = ajv.compile(dateArgsSchema);
 
-var validArgs = require('./valid_args');
-var invalidArgs = require('./invalid_args');
+var validArgs = require("./valid_args");
+var invalidArgs = require("./invalid_args");
 
-validArgs.forEach(function (args, i) {
-  assert(validate(args), '#' + i + ' should be valid: ' + JSON.stringify(args));
+validArgs.forEach(function(args, i) {
+  assert(validate(args), "#" + i + " should be valid: " + JSON.stringify(args));
 });
 
-
-invalidArgs.forEach(function (args, i) {
-  assert(!validate(args), '#' + i + ' should be invalid: ' + JSON.stringify(args));
+invalidArgs.forEach(function(args, i) {
+  assert(
+    !validate(args),
+    "#" + i + " should be invalid: " + JSON.stringify(args)
+  );
 });
 
-console.log('You\'ve done task 3!');
+console.log("You've done task 3!");
